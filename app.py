d79e17831bfa18811386deddddc2e74bdd1feed2
Maria Stepanova <116209851+MNJMARIA@users.noreply.github.com>
2024-08-19 21:40:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@
 from flask import Flask, request, jsonify
 from telethon import TelegramClient
 import asyncio
-import threading
+#import threading
 
 app = Flask(__name__)
 
@@ -31,9 +31,16 @@ phone = '+380936707972'  # Your phone number
 
 client = TelegramClient('birthday_greetings_session', api_id, api_hash)
 
+#async def send_message(phone_number, message):
+#    async with client:
+#        await client.send_message(phone_number, message)
+
+
 async def send_message(phone_number, message):
-    async with client:
-        await client.send_message(phone_number, message)
+    await client.start(phone)
+    await client.send_message(phone_number, message)
+    await client.disconnect()
+
 
 @app.route('/')
 def hello_world():
@@ -50,14 +57,22 @@ def send_telegram_message():
     if not phone_number or not message:
         return jsonify({'status': 'error', 'message': 'Missing phone_number or message'}), 400
 
-    try:
-        # Run the async function in a separate thread to avoid blocking the Flask server
-        loop = asyncio.new_event_loop()
-        threading.Thread(target=lambda: loop.run_until_complete(send_message(phone_number, message))).start()
+    #try:
+    #    # Run the async function in a separate thread to avoid blocking the Flask server
+    #    loop = asyncio.new_event_loop()
+    #    threading.Thread(target=lambda: loop.run_until_complete(send_message(phone_number, message))).start()
+    #    return jsonify({'status': 'success', 'message': f'Message sent to {phone_number}'})
+    #except Exception as e:
+    #    print(f"Exception: {e}")
+    #    return jsonify({'status': 'error', 'message': str(e)}), 500
+
+ try:
+        asyncio.run(send_message(phone_number, message))  # Викликаємо асинхронний код без потоків
         return jsonify({'status': 'success', 'message': f'Message sent to {phone_number}'})
     except Exception as e:
         print(f"Exception: {e}")
         return jsonify({'status': 'error', 'message': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
