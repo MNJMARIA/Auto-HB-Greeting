69bd30d0a3798f237203b56042188320a88ae83d
Maria Stepanova <116209851+MNJMARIA@users.noreply.github.com>
2024-08-20 12:05:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,14 +28,18 @@ phone = '+380936707972'  # Ваш номер телефону
 
 client = TelegramClient('birthday_greetings_session', api_id, api_hash)
 
-async def send_message(phone_number, message):
-    await client.start(phone)
-    await client.send_message(phone_number, message)
-    await client.disconnect()
+#async def send_message(phone_number, message):
+#    await client.start(phone)
+#    await client.send_message(phone_number, message)
+#    await client.disconnect()
 
+async def send_message(phone_number, message):
+    async with client:
+        await client.send_message(phone_number, message)
+        
 @app.route('/')
 def hello_world():
-    return 'Hellooo, World!'
+    return 'Hellooooo, World!'
 
 
 @app.route('/send_greeting', methods=['POST'])
@@ -54,9 +58,13 @@ def send_greeting():
     #except Exception as e:
     #    app.logger.error(f"Error sending message: {e}")
     #    return jsonify({"status": "error", "message": f"Failed to send message: {str(e)}"}), 500
-    with client:
-        client.loop.run_until_complete(send_message(phone_number, message))
-    return jsonify({"status": "success", "message": f"Greeting sent to {phone_number}"})
+    try:
+        asyncio.run(send_message(phone_number, message))
+        return jsonify({"status": "success", "message": f"Greeting sent to {phone_number}"})
+    except Exception as e:
+        app.logger.error(f"Error sending message: {e}")
+        return jsonify({"status": "error", "message": f"Failed to send message: {str(e)}"}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
